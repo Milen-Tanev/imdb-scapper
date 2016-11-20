df5036e18dce33fe3622c06c5fd3cec59fcf61fa
Viki94 <viktoria.nedyalkova94@gmail.com>
2016-11-20 22:30:31 +0200

    Add constants

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,13 @@
 module.exports = {
     connectionString: "mongodb://localhost/moviesDb",
     genres: ["action", "sci-fi", "fantasy", "horror", "comedy"],
-    pagesCount: 50
+    pagesCount: 50,
+    actorSelector: {
+        profileImage: "#name-poster",
+        name: "#overview-top .header span",
+        biography: "#name-bio-text .inline",
+        moviesName: "#filmography .filmo-category-section:first .filmo-row b a",
+        moviesImdbId: "#filmography .filmo-category-section:first .filmo-row b a",
+        moviesCharacterName: "#filmography .filmo-category-section:first .filmo-row"
+    }
 };
